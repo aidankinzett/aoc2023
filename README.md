915227d8d5562ff7fbea1a9d2b419b6453973a27
Aidan Kinzett <aidan@labrys.io>
2023-12-11 16:48:58 +1000

    day 4

--- README.md
+++ README.md
@@ -11,10 +11,10 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
+![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
+![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: -
 
 ```
 Day 03
-Time part 1: 1.831ms
+Time part 1: 1.678ms
 Time part 2: -
-Both parts: 1.831ms
+Both parts: 1.678ms
 ```
 
 ```
 Day 04
-Time part 1: -
+Time part 1: 1.519ms
 Time part 2: -
-Both parts: -
+Both parts: 1.519ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 1.831ms
+Total stars: 2/50
+Total time: 3.197ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day04/index.ts
@@ -0,0 +1,66 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+/**
+ * Get points from the number of winning numbers
+ * @param number Number of winning numbers
+ * @returns Points
+ */
+const points = (number: number) => number > 0 ? 2**(number - 1) : 0;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput).split("\n");
+
+  let sum = 0;
+  input.forEach((line) => {
+    const [_, numbers] = line.split(": ");
+    const [winningNumbers, ticketNumbers] = numbers.split(" | ");
+
+    const winningNumbersList = [...winningNumbers.matchAll(/\d+/g)].map(v => parseInt(v[0]));
+
+
+    const ticketNumbersList = [...ticketNumbers.matchAll(/\d+/g)].map(v => parseInt(v[0]));
+
+    // how many ticket numbers are winning numbers
+    const winningNumbersCount = ticketNumbersList.filter((n) => winningNumbersList.includes(n)).length;
+
+    sum += points(winningNumbersCount);
+  });
+
+  return sum;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 13,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
